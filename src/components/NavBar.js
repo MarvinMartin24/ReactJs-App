bbc8021bc5fc6d302367f85b0e1ebdb99e60c4b6
MarvinMartin24 <mmartin.mmarvin@gmail.com>
2019-10-16 20:26:22 +0200

    add logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import logo from '../logo.png';
+
 
 
 const NavBar = () => {
   return (
     <div className="container">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
       <a href="/"> Accueil </a>
       <br />
       <a href="/LogIn"> Log In </a>
