3f7beeed1c45a831e2acf54c3426c61cbc7a5787
MarvinMartin24 <mmartin.mmarvin@gmail.com>
2019-10-24 15:03:13 +0200

    Clean SignUp

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,84 +3,85 @@ import * as api from '../services/apiService.js';
 import { existingId, createId } from '../services/api/server.js';
 
 
-
 class SignUp extends Component {
 
-
     constructor(props) {
-    super(props);
+        super(props);
+        existingId();
 
-    existingId();
-
-    this.state = {
-      id: createId(),
-      first_name: '',
-      last_name: '',
-      email: '',
-      password: ''
-    }
-  }
-
-
-  handleChange = (event) => {
-     const input = event.target;
-     const value = input.value;
-
-     this.setState({ [input.id]: value })
-  }
-
-
-  submit = (event) => {
-      event.preventDefault();
-
-    const signUpUser = this.state;
-
-    if(api.isEmailAvailable(signUpUser.email)){
-
-        api.createUser(signUpUser)
-
-        localStorage.setItem('user_local', JSON.stringify(signUpUser));
-
-        alert("Account Created !")
-
-        this.props.history.push("/LogIn");
-
-    }
-    else {
-        alert("Email Already used !")
+        this.state = {
+            id: createId(),
+            first_name: '',
+            last_name: '',
+            email: '',
+            password: ''
+        };
     }
 
-  }
+    handleChange = (event) => {
 
-  render() {
-    return (
-      <div >
-      <form onSubmit={this.submit}>
-        <label>First Name:</label>
-        <input id="first_name" type="text" onChange={this.handleChange}/>
+        var input = event.target;
+        var value = input.value;
 
-        <br/>
+        this.setState({
+            [input.id]: value
+        });
+    }
+
+    submit = (event) => {
+
+        event.preventDefault();
+
+        var signUpUser = this.state;
+        var available = api.isEmailAvailable(signUpUser.email);
+
+        if (available) {
+
+            api.createUser(signUpUser)
+
+            localStorage.setItem('user_local', JSON.stringify(signUpUser));
+
+            alert('Account Created !')
+
+            this.props.history.push('/LogIn');
+        }
+        else {
+
+            alert('Email Already used !')
+        }
+    }
 
 
-        <label>Last Name:</label>
-        <input id="last_name" type="text" onChange={this.handleChange}/>
+    render() {
 
-        <br/>
+        return (
+            <div >
+                <form onSubmit={this.submit}>
+                    <label>First Name:</label>
+                    <input id="first_name" type="text" onChange={this.handleChange}/>
 
-        <label>Email:</label>
-        <input id="email" type="text" onChange={this.handleChange}/>
+                    <br/>
 
-        <br/>
+                    <label>Last Name:</label>
+                    <input id="last_name" type="text" onChange={this.handleChange}/>
 
-        <label>Password:</label>
-        <input id="password" type="password" onChange={this.handleChange}/>
+                    <br/>
 
-        <br/>
-        <button >Create Account</button>
-      </form>
-      </div>
-    );
-  }
+                    <label>Email:</label>
+                    <input id="email" type="text" onChange={this.handleChange}/>
 
+                    <br/>
+
+                    <label>Password:</label>
+                    <input id="password" type="password" onChange={this.handleChange}/>
+
+                    <br/>
+
+                    <button >Create Account</button>
+                </form>
+            </div>
+        );
+    }
 }
+
 export default SignUp;
