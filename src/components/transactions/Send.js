525963f14f0b0080ddfe5bf54ac3123756fff6ea
MarvinMartin24 <mmartin.mmarvin@gmail.com>
2019-10-26 20:16:33 +0200

    send check mail

--- src/components/transactions/Send.js
+++ src/components/transactions/Send.js
@@ -10,11 +10,13 @@ class Send extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            id: api.getNewIdTransfer(),
-            debited_wallet_id: this.props.walletId,
-            credited_wallet_id:'',
-            amount: 0,
-            listTransfers: api.getTransfers(this.props.walletId),
+            transfer: {
+                id: api.getNewIdTransfer(),
+                debited_wallet_id: this.props.walletId,
+                credited_wallet_id:'',
+                amount: 0,
+            },
+            listTransfers: api.getTransfers(this.props.walletId)
         };
     }
 
@@ -22,7 +24,12 @@ class Send extends Component {
         event.preventDefault();
 
         this.setState({
-            amount: parseInt(event.target.value)*100,
+            transfer: {
+                id: this.state.transfer.id,
+                debited_wallet_id: this.props.walletId,
+                credited_wallet_id: api.getWalletIdFromEmail(event.target.value),
+                amount: parseInt(event.target.value)*100,
+            }
         });
     }
 
@@ -30,12 +37,17 @@ class Send extends Component {
         event.preventDefault();
 
         this.setState({
-            credited_wallet_id: api.getWalletIdFromEmail(event.target.value),
+            transfer: {
+                id: this.state.transfer.id,
+                debited_wallet_id: this.props.walletId,
+                credited_wallet_id: api.getWalletIdFromEmail(event.target.value),
+                amount: this.state.transfer.amount
+            }
         })
     }
 
     displayTransfers = () => {
-        const listItems = this.state.listTransfers.map((transfer) =>
+        const listItems = this.state.transfer.listTransfers.map((transfer) =>
             <li>{transfer}</li>
         );
         return <ul>{listItems}</ul>;
@@ -44,7 +56,7 @@ class Send extends Component {
 
     isEmailValid = () => {
 
-        if ((this.state.credited_wallet_id !== this.props.walletId) && (this.state.credited_wallet_id.status !== 'failure')){
+        if ((this.state.transfer.credited_wallet_id !== this.props.walletId) && (this.state.transfer.credited_wallet_id.status !== 'failure')){
             return(true);
         }
         else{
@@ -54,7 +66,7 @@ class Send extends Component {
     }
 
     isAmountValid = () => {
-        if (this.state.amount <= this.props.solde){
+        if (this.state.transfer.amount <= this.props.solde){
             return(true);
         }
         else{
@@ -68,8 +80,8 @@ class Send extends Component {
 
         if ((this.isEmailValid()) && (this.isAmountValid())){
 
-            api.addTransfert(this.state);
-            api.updateWallets(this.state.debited_wallet_id, this.state.credited_wallet_id, this.state.amount);
+            api.addTransfert(this.state.transfer);
+            api.updateWallets(this.state.transfer.debited_wallet_id, this.state.transfer.credited_wallet_id, this.state.transfer.amount);
             this.props.onChange()
             this.setState({
                 listTransfers: api.getTransfers(this.props.walletId),
@@ -77,12 +89,10 @@ class Send extends Component {
 
             alert('Transaction done !');
         }
-
-
     }
 
     render() {
-        console.log(this.state);
+        console.log(this.state.transfer);
         return (
             <div>
                 <form className="send-form" onSubmit={this.transfer}>
@@ -105,7 +115,7 @@ class Send extends Component {
                 <div className="display-page">
                     <div className="display-form">
                         <Form.Group controlId="exampleForm.ControlSelect1">
-                            <Form.Label>Transfers list :</Form.Label>
+                            <Form.Label className="text-transfers">Transfers</Form.Label>
                                 {
                                     this.state.listTransfers.map((transfer, index) => (
                                             <option key={index}>
