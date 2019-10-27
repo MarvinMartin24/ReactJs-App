7a1f3fc4c7dc7ba29ccfa01c423b8b9c62223b26
DuyN91 <36534618+DuyN91@users.noreply.github.com>
2019-10-27 23:30:22 +0100

    card brand

--- src/services/api/json/cards.js
+++ src/services/api/json/cards.js
@@ -3,28 +3,28 @@ export const cards = [
             user_id: "1",
             id:"1",
             last_4:"1111",
-            brand:"Visa",
+            brand:"visa",
             expired_at:"2022-09-24"
     },
     {
             user_id: "2",
             id:"2",
             last_4:"2222",
-            brand:"Master",
+            brand:"master_card",
             expired_at:"2021-10-11"
     },
     {
             user_id: "1",
             id:"3",
             last_4:"7777",
-            brand:"Master",
+            brand:"american_express",
             expired_at:"2021-03-27"
     },
     {
             user_id: "3",
             id:"4",
             last_4:"3333",
-            brand:"Revolut",
+            brand:"visa",
             expired_at:"2020-03-27"
     }
 
